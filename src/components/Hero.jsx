768c1749f572a97da5b22b36eb036e933bba8f7e
medsonmoombe <medsonmoombe21@gmail.com>
2023-03-13 21:09:15 +0200

    Add the meadia query to only show the 3d image on big screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import { motion } from "framer-motion";
-import { Link } from "react-router-dom";
+import { useMediaQuery } from '@react-hook/media-query';
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const media = useMediaQuery('(max-width: 767px)');
+
   return (
     <section className="relative w-full h-screen mv:h-[750px] mx-auto">
       <div className="absolute inset-0 top-[120px] max-w-7xl mx-auto px-5 py-10 flex flex-row items-start gap-5">
@@ -13,26 +15,33 @@ const Hero = () => {
           <div className="w-1 sm:h-80 h-40 bg-gradient-to-br from-[#715AFF] to-[#915EFF]" />
         </div>
 
-<div className="z-10 mv:gap-2 gap-4 w-1/2 mv:w-[100%]">
-  <h1 className="text-4xl mv:text-xl md:text-5xl font-bold leading-tight text-white mb-2 w-1/2 sm:w-[100%]">
-    Hi, I'm <span className="text-[#915EFF]">Emmanuel</span>
-  </h1>
-  <p className="text-lg md:text-xl text-gray-200 leading-relaxed mb-8 w-4/5 mv:text-sm">
-    Full-stack developer crafting beautiful, efficient, and scalable web applications with expertise in JavaScript, Node.js, Ruby on Rails, and SQL.
-  </p>
-  <div className="mt-10">
-    <a
-      href="#contact"
-      className="px-8 py-3 mv:px-4 mv:py-3 mv:text-sm mv: cursor-pointer rounded-lg bg-[#915EFF] text-white hover:bg-opacity-80 transition duration-300 ease-in-out"
-    >
-      Get in Touch
-    </a>
-  </div>
-</div>
-
+        <div className="z-10 mv:gap-2 gap-4 w-1/2 mv:w-[100%]">
+          <h1 className="text-4xl mv:text-xl md:text-5xl font-bold leading-tight text-white mb-2 w-1/2 mv:w-[100%]">
+            Hi, I'm <span className="text-[#915EFF]">Emmanuel</span>
+          </h1>
+          <p className="text-lg md:text-xl text-gray-200 leading-relaxed mb-8 w-4/5 mv:text-sm">
+            Full-stack developer crafting beautiful, efficient, and scalable web
+            applications with expertise in JavaScript, Node.js, Ruby on Rails,
+            and SQL.
+          </p>
+          <div className="mt-10">
+            <a
+              href="#contact"
+              className="px-8 py-3 mv:px-4 mv:py-3 mv:text-sm mv: cursor-pointer rounded-lg bg-[#915EFF] text-white hover:bg-opacity-80 transition duration-300 ease-in-out"
+            >
+              Get in Touch
+            </a>
+          </div>
+        </div>
       </div>
 
-      <ComputersCanvas />
+      {!media ? (
+        <ComputersCanvas />
+      ) : (
+        <div class="mv:hidden">
+          <ComputersCanvas />
+        </div>
+      )}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
